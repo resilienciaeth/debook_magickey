b510d069b83386316ce450dece6494cb1f55c24b
resilienciaeth <106503220+resilienciaeth@users.noreply.github.com>
2022-12-27 12:23:27 -0500

    wip:first-build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/drxuutjwr',
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+const withFonts = require('next-fonts');
 
-module.exports = nextConfig
+module.exports = withFonts({
+  webpack(config, options) {
+    config.node = {
+      fs: 'empty',
+    };
+    config.module.rules.push({
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: 'url-loader?limit=100000',
+        },
+        {
+          loader: 'file-loader',
+        },
+      ],
+    });
+    return config;
+  },
+});
+
+module.exports = nextConfig;
